ead6ee439ae6a081ae2169160693a1334cc75896
Emilia <maria.artola@alumnos.utn.frm.edu.argit config --global user.email maria.artola@alumnos.frm.utn.edu.ar>
2021-05-22 22:04:09 -0300

    Clase Jugador

---
+++ Proyecto/Player.py
@@ -0,0 +1,9 @@
+class PlayerClass:
+
+    def __init__(self, player):
+        player.name = ''  #Personaje que le toca
+        player.lives = 10
+        player.energy = 0
+        player.victoryPoints = 0
+        player.cards = []
+        player.position = int
